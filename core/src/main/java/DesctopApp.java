fef02913f8a74ca883d7c399567be491690a9ff6
slonikmak <slonikmak@gmail.com>
2017-01-23 17:56:04 +0300

    init

--- core/src/main/java/DesctopApp.java
+++ core/src/main/java/DesctopApp.java
@@ -1,10 +1,15 @@
+import config.GuiConfiguration;
 import mainApp.GuiApp;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 /**
  * Created by Oceanos on 23.01.2017.
  */
 public class DesctopApp {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(GuiConfiguration.class);
         GuiApp.main(args);
     }
 }
---
+++ gui/src/main/java/mainApp/NewApp.java
@@ -0,0 +1,8 @@
+package mainApp;
+
+/**
+ * Created by Oceanos on 23.01.2017.
+ */
+public class NewApp {
+
+}
